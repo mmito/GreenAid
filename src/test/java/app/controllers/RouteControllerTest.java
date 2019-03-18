b3d5f8362137dbafdfb090378a9e5664083b5d85
Sven van Collenburg <S.D.vanCollenburg@student.tudelft.nl>
2019-03-18 02:31:52 +0100

    the RouteControllerTests that can be done without refactoring

--- src/test/java/app/controllers/RouteControllerTest.java
+++ src/test/java/app/controllers/RouteControllerTest.java
@@ -1,6 +1,10 @@
 package app.controllers;
 
 import app.authentication.SecurityServiceImpl;
+import app.repository.ActivityRepository;
+import app.repository.CategoryRepository;
+import app.repository.FriendshipRepository;
+import app.services.ActivityServiceImpl;
 import app.services.UserServiceImpl;
 import app.models.User;
 import app.repository.UserRepository;
@@ -9,6 +13,7 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -44,86 +49,119 @@ public class RouteControllerTest {
     @MockBean
     private UserRepository userRepository;
 
+    @MockBean
+    private FriendshipRepository friendshipRepository;
+
+    @MockBean
+    private ActivityServiceImpl activityService;
+
+    @MockBean
+    private CategoryRepository categoryRepository;
+
+    @MockBean
+    private ActivityRepository activityRepository;
 
     @Test
-    public void welcomeEndpointIncorrectUserCredentials() throws Exception {
-
-        HttpHeaders headers = new HttpHeaders();
-
-        String authHeader = getAuthenticationHeader();
-        headers.set("Authorization", authHeader);
-
-        // https://spring.io/blog/2016/04/15/testing-improvements-in-spring-boot-1-4
-
-        this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON).headers(headers))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrlPattern("http://*/login"));
-    }
-
-    @Test
-    @WithMockUser(value = "spring")
-    public void welcomeEndpointValidUserCredentials() throws Exception {
+    public void clientErrorOnRoot() throws Exception {
 
         this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("welcome"));
-    }
-
-    @Test
-    public void registrationGetRequest() throws Exception {
-
-        this.mockMvc.perform(get("/registration").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string("registration"));
+                .andExpect(status().is4xxClientError());
     }
 
     @Test
     @WithMockUser(value = "spring")
-    public void registrationPostRequest() throws Exception {
+    public void welcomeEndpoint() throws Exception {
 
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        User userForm = new User();
-        userForm.setUsername("aName");
-        userForm.setPassword("aPassword");
-        //userForm.setPasswordConfirm("aPassword");
-        String requestJson = ow.writeValueAsString(userForm);
-
-
-        this.mockMvc.perform(
-            post("/registration")
-                        .accept(MediaType.APPLICATION_JSON_VALUE)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestJson)
-        )
+        this.mockMvc.perform(get("/welcome").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string("redirect:/welcome"));
-
+                .andExpect(content().string("{\"ok\":true,\"data\":\"welcome\"}"));
     }
 
-    /*@Test
+    @Test
     @WithMockUser(value = "spring")
-    public void activityEndpointPost() throws Exception {
+    public void userfirstEndpoint() throws Exception {
 
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        Activity activity = new Activity("car", "100");
-        String requestJson = ow.writeValueAsString(activity);
+        User u = new User();
+        u.setFirst_name("John");
+        Mockito.when(securityService.findLoggedInUsername()).thenReturn("abc");
+        Mockito.when(userService.findByUsername("abc")).thenReturn(u);
 
-        this.mockMvc.perform(
-                post("/activities")
-                        .accept(MediaType.APPLICATION_JSON_VALUE)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestJson)
-        )
+
+        this.mockMvc.perform(get("/userfirst").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Activity information saved successfully: car 100"));
-
-    }*/
-
-
-    private String getAuthenticationHeader() {
-        String auth = USER_NAME + ":" + PASSWORD;
-        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-        return "Basic " + new String(encodedAuth);
+                .andExpect(content().string("{\"ok\":true,\"data\":\"John\"}"));
     }
+
+    @Test
+    @WithMockUser(value = "spring")
+    public void userlastEndpoint() throws Exception {
+
+        User u = new User();
+        u.setLast_name("Smith");
+        Mockito.when(securityService.findLoggedInUsername()).thenReturn("abc");
+        Mockito.when(userService.findByUsername("abc")).thenReturn(u);
+
+
+        this.mockMvc.perform(get("/userlast").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("{\"ok\":true,\"data\":\"Smith\"}"));
+    }
+
+//    @Test
+//    public void registrationGetRequest() throws Exception {
+//
+//        this.mockMvc.perform(get("/registration").accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("registration"));
+//    }
+
+//    @Test
+//    @WithMockUser(value = "spring")
+//    public void registrationPostRequest() throws Exception {
+//
+//        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+//        User userForm = new User();
+//        userForm.setUsername("aName");
+//        userForm.setPassword("aPassword");
+//        //userForm.setPasswordConfirm("aPassword");
+//        String requestJson = ow.writeValueAsString(userForm);
+//
+//
+//        this.mockMvc.perform(
+//                post("/registration")
+//                        .accept(MediaType.APPLICATION_JSON_VALUE)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(requestJson)
+//        )
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("redirect:/welcome"));
+//
+//    }
+
+//    @Test
+//    @WithMockUser(value = "spring")
+//    public void activityEndpointPost() throws Exception {
+//
+//        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+//        Activity activity = new Activity("car", "100");
+//        String requestJson = ow.writeValueAsString(activity);
+//
+//        this.mockMvc.perform(
+//                post("/activities")
+//                        .accept(MediaType.APPLICATION_JSON_VALUE)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(requestJson)
+//        )
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("Activity information saved successfully: car 100"));
+//
+//    }
+
+
+//    private String getAuthenticationHeader() {
+//        String auth = USER_NAME + ":" + PASSWORD;
+//        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+//        return "Basic " + new String(encodedAuth);
+//    }
 
 }
