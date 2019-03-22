b0e9ae6670e1f2f38a637d2a2ab585d054f035bb
cpene <pene.cosmin.octavian@gmail.com>
2019-03-22 03:43:09 +0100

    tests for HttpRequest class (need improvements)

--- src/test/java/app/client/HttpRequestsTest.java
+++ src/test/java/app/client/HttpRequestsTest.java
@@ -4,14 +4,19 @@ import app.responses.Response;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment  = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class HttpRequestsTest {
 
     HttpRequests httpRequests;
