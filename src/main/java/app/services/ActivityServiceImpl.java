1a38ccff2e52b8324e6f3457910f5943e29a2751
Sven van Collenburg <S.D.vanCollenburg@student.tudelft.nl>
2019-03-31 23:01:23 +0200

    refactored another method from the UserController

--- src/main/java/app/services/ActivityServiceImpl.java
+++ src/main/java/app/services/ActivityServiceImpl.java
@@ -1,10 +1,13 @@
 package app.services;
 
 import app.models.Activity;
+import app.models.ActivityProjection;
+import app.models.User;
 import app.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -20,7 +23,6 @@ public class ActivityServiceImpl {
     public void save(Activity activity) {
 
         activityRepository.save(activity);
-
     }
 
     /**
@@ -30,7 +32,6 @@ public class ActivityServiceImpl {
     public void delete(Activity activity) {
 
         activityRepository.delete(activity);
-
     }
 
     /**
@@ -41,7 +42,6 @@ public class ActivityServiceImpl {
     public List<Activity> findByUser_id(long userId) {
 
         return activityRepository.findByUser_id(userId);
-
     }
 
     /**
@@ -52,7 +52,44 @@ public class ActivityServiceImpl {
     public Activity findById(long id) {
 
         return activityRepository.findById(id);
-
     }
 
+    public List<ActivityProjection> getActivities(User user) {
+
+        List<Activity> activities = activityRepository.findByUser_id(user.getId());
+        List<ActivityProjection> activityArray = new LinkedList<>();
+
+        for (Activity a : activities) {
+
+            long id = a.getId();
+            double amount = a.getAmount();
+            double xp_points = a.getXp_points();
+            String category;
+            switch ((int) a.getCategory_id()) {
+
+                case 1:
+                    category = "Eating a vegetarian meal";
+                    break;
+                case 2:
+                    category = "Buying local produce";
+                    break;
+                case 3:
+                    category = "Using bike instead of car";
+                    break;
+                case 4:
+                    category = "Using public transport instead of car";
+                    break;
+                case 5:
+                    category = "Installing solar panels";
+                    break;
+                case 6:
+                    category = "Lowering the temperature of your home";
+                    break;
+                default:
+                    category = "unknown";
+            }
+            activityArray.add(new ActivityProjection(id, user.getUsername(), category, amount, xp_points));
+        }
+        return activityArray;
+    }
 }
