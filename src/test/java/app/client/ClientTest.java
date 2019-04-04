d92667d8bba89eef9a8015bccd74db82a6993774
Sven van Collenburg <S.D.vanCollenburg@student.tudelft.nl>
2019-04-04 23:13:26 +0200

    another client test

--- src/test/java/app/client/ClientTest.java
+++ src/test/java/app/client/ClientTest.java
@@ -1,7 +1,10 @@
 package app.client;
 
 
+import app.models.ActivityProjection;
 import app.responses.Response;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
@@ -12,6 +15,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -77,6 +83,32 @@ public class ClientTest {
         HttpRequests.postRequest("cookie", "http://localhost:8080/user/add-activity", params);
 
         assertEquals("responseTxt", response);
+    }
+
+    @Test
+    public void getUserActivities() {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<ActivityProjection> actList = new ArrayList<>();
+        ActivityProjection ap = new ActivityProjection(12, "user", "cat", 3.14, 12.1);
+        actList.add(ap);
+        JsonNode body = mapper.convertValue(actList, JsonNode.class);
+
+        HttpEntity<Response> mockResponse = new HttpEntity<>(new Response(), null);
+        mockResponse.getBody().setOk(true);
+        mockResponse.getBody().setData(body);
+
+        PowerMockito.mockStatic(HttpRequests.class);
+        PowerMockito.when(HttpRequests.getRequest("cookie", "http://localhost:8080/user/activities"))
+                .thenReturn(mockResponse);
+
+        Object response = Client.getUserActivities("cookie");
+
+        assertEquals(actList, response);
+
+        PowerMockito.verifyStatic(HttpRequests.class);
+        HttpRequests.getRequest("cookie", "http://localhost:8080/user/activities");
     }
 
 }
